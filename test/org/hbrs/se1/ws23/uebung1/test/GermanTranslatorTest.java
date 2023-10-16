e0eca04fd88191e1eaf71d9be5a07af93955b384
Daniel <a@y.com>
2023-10-16 23:00:45 +0200

    .

--- test/org/hbrs/se1/ws23/uebung1/test/GermanTranslatorTest.java
+++ test/org/hbrs/se1/ws23/uebung1/test/GermanTranslatorTest.java
@@ -2,26 +2,27 @@ package org.hbrs.se1.ws23.uebung1.test;
 
 import org.hbrs.se1.ws23.uebung1.control.GermanTranslator;
 import org.hbrs.se1.ws23.uebung1.control.Translator;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GermanTranslatorTest {
+    private GermanTranslator translator = new GermanTranslator();
 
     @Test
-    void aPositiveTest() {
-        GermanTranslator translator = new GermanTranslator();
-
+    void positive_num() {
         String pos_ak1 = translator.translateNumber(1);
         assertEquals(pos_ak1, "eins");
-
-        String neg_ak2 = translator.translateNumber(-1);
-        assertEquals(neg_ak2,"Übersetzung der Zahl [" + -1 + "] nicht\n" +
-                "möglich ([" + Translator.version + "])");
 
         String pos_ak3 = translator.translateNumber(100);
         assertEquals(pos_ak3, "Übersetzung der Zahl [" + 100 + "] nicht\n" +
                 "möglich ([" + Translator.version + "])");
-
+    }
+    @Test
+    void negative_num(){
+        String neg_ak2 = translator.translateNumber(-1);
+        assertEquals(neg_ak2,"Übersetzung der Zahl [" + -1 + "] nicht\n" +
+                "möglich ([" + Translator.version + "])");
     }
 }
